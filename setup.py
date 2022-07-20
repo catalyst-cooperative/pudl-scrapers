978ce35099240f7b543473c2bb87b497868cab72
t-desktop <tpbush@gmail.com>
2022-07-19 22:27:31 -0700

    add draft eia_api to scraping script

--- setup.py
+++ setup.py
@@ -3,8 +3,15 @@
 
 from setuptools import find_packages, setup
 
-setup(name="pudl_scrapers",
-      version="0.2.0",
-      author="PUDL",
-      packages=find_packages(),
-      entry_points={"console_scripts": ["epacems=pudl_scrapers.bin.epacems:main"]})
+setup(
+    name="pudl_scrapers",
+    version="0.2.0",
+    author="PUDL",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "epacems=pudl_scrapers.bin.epacems:main",
+            "eia_api=pudl_scrapers.bin.eia_api:main",
+        ]
+    },
+)
